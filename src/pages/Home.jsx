1b4a6d7ae3249f91f69ad89ac76eac5fb072d868
Azizbek <khasanjonovich@gmail.com>
2022-03-02 04:59:33 +0900

    intro

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,12 @@
 import { Carousel } from "../components/Carousel";
-// import { Intro } from "../components/Intro";
+import { Intro } from "../components/Intro";
 import { Footer } from "../components/Footer";
 
 export const Home = () => {
   return (
     <div className="home">
       <Carousel></Carousel>
-      {/* <Intro></Intro> */}
+      <Intro></Intro>
       <Footer></Footer>
     </div>
   );
