85092fc548abfffc84306ee11e5d1cd3f92030b4
Azizbek <khasanjonovich@gmail.com>
2022-03-02 01:14:31 +0900

    project init

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { Home } from "./pages/Home";
+import { Header } from "./components/Header";
+function App() {
+  return (
+    <div className="App">
+      <Header></Header>
+      <Home></Home>
+    </div>
+  );
+}
+
+export default App;
