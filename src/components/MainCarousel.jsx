191edeb1a5ce3baa20061a53d522706b9e7643a5
Azizbek <khasanjonovich@gmail.com>
2022-03-06 08:02:05 +0900

    main carousel

---
+++ src/components/MainCarousel.jsx
@@ -0,0 +1,37 @@
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/navigation";
+
+// import required modules
+import { Autoplay, Navigation } from "swiper";
+
+export const MainCarousel = () => {
+  return (
+    <>
+      <Swiper
+        navigation={true}
+        autoplay={{
+          delay: 2500,
+          disableOnInteraction: false,
+        }}
+        modules={[Autoplay, Navigation]}
+        className="main-carousel"
+      >
+        <SwiperSlide>
+          <img src="img/we-are-creative.jpg" alt="" />
+        </SwiperSlide>
+        <SwiperSlide>Slide 2</SwiperSlide>
+        <SwiperSlide>Slide 3</SwiperSlide>
+        <SwiperSlide>Slide 4</SwiperSlide>
+        <SwiperSlide>Slide 5</SwiperSlide>
+        <SwiperSlide>Slide 6</SwiperSlide>
+        <SwiperSlide>Slide 7</SwiperSlide>
+        <SwiperSlide>Slide 8</SwiperSlide>
+        <SwiperSlide>Slide 9</SwiperSlide>
+      </Swiper>
+    </>
+  );
+};
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,12 @@
 import { Intro } from "../components/Intro";
 import { Tracks } from "../components/Tracks";
 import { Showcase } from "../components/Showcase";
+import { MainCarousel } from "../components/MainCarousel";
 
 export const Home = () => {
   return (
     <div className="home">
-      {/* <Carousel /> */}
+      <MainCarousel></MainCarousel>
       <Intro />
       <Tracks />
       <Showcase />
