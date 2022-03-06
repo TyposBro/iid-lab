c1355385a1c1904793cc977dff27f28c244bfbd1
Azizbek <khasanjonovich@gmail.com>
2022-03-06 12:19:39 +0900

    people

--- src/components/Person.jsx
+++ src/components/Person.jsx
@@ -1,22 +1,19 @@
-export const Person = () => {
+export const Person = ({ person }) => {
+  console.log(person);
   return (
     <div className="person">
-      <div className="person__title">director</div>
-      <div className="person__info-box">
-        <div className="person__img-box">{/* <img src="" alt="" className="person__img" /> */}</div>
-        <div className="person__info">
-          <h3 className="person__name">KwanMyung Kim</h3>
-          <div className="person__degree">Professor</div>
-          <div className="person__email">kmyung@unist.ac.kr </div>
-          <div className="person__phone">+82-52-217-2714</div>
-        </div>
-        <div className="person__bio">
-          Dr. KwanMyung Kim is a full professor in Department of Design and the director of Integration and Innovation Design Lab (IIDL). He was a Dean of Graduate School of Creative Design Engineering during 2016-2020.
-          He serves as an editor for Archives of Design Research and ICONARP International Journal of Architecture and Planning. He is also a CEO of ID SPACE Corp., a start-up company that commercializes academic
-          research outcomes. Before joining UNIST, he worked in industry for 14 years as a product designer/engineer. With his strong practical knowdge and experience he persues to integrate design and engineering, and
-          industry and academic knowledge in the major IIDL's research domains including Design for Elderly, Rehabilitation and Health Care.
-          <button className="person__bio-more">check for more</button>
-        </div>
+      <div className="person__img-box">
+        <img src={person.img} alt="" className="person__img" />
+      </div>
+      <div className="person__info">
+        <h3 className="person__name">{person.name}</h3>
+        <h4 className="person__detail">{person.title}</h4>
+        <h4 className="person__detail">{person.email} </h4>
+        <h4 className="person__detail">{person.phone}</h4>
+      </div>
+      <div className="person__bio">
+        {person.bio}
+        {/* <button className="person__more">check for more</button> */}
       </div>
     </div>
   );
--- src/pages/People.jsx
+++ src/pages/People.jsx
@@ -1,18 +1,24 @@
 import { Person } from "../components/Person";
+import data from "../data.json";
 export const People = () => {
+  const people = data.PEOPLE.PhD.map((e) => <Person person={e}></Person>);
   return (
     <div className="people">
       <div className="people__img-box">
         <img className="people__img" src="img/team.jpg" alt="" />
       </div>
       <div className="people__content">
-        <Person className="people__person"></Person>
+        <div className="people__list">
+          <h3 className="people__heading">director</h3>
+          {people}
+        </div>
+        {/* <Person person={data.PEOPLE.Prof[0]}></Person> */}
         <div className="section">
           <div className="section__logo-wrap">
             <img src="img/logo.jpg" alt="" className="section__logo" />
           </div>
           <ul className="section__list">
-            <li className="section__item section_active">Director</li>
+            <li className="section__item section__active">Director</li>
             <li className="section__item">PhD</li>
             <li className="section__item">Masters</li>
             <li className="section__item">Interns</li>
