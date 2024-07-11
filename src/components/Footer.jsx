ff1d5c547ba08ba5c666af5ab7e63d43b8448ce3
typosbro <typosbro@proton.me>
2024-07-12 01:01:46 +0900

    footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,10 +5,10 @@ import Logo from "../assets/footer_logo.svg";
 
 export const Footer = () => {
   return (
-    <footer className="p-16 bg-[#231F20] grid grid-cols-2 grid-rows-2 gap-y-16 font-regular">
+    <footer className="p-[30px] bg-[#231F20] grid grid-cols-2 grid-rows-2 gap-y-16 font-regular">
       {/* CTA */}
       <div className="flex flex-col gap-4">
-        <h1 className="text-2xl text-white font-special text-[80px] font-bold leading-[72px] font-light">
+        <h1 className="text-white font-special text-[40px] leading-[72px] font-light">
           Contact <span className="text-[#25AAE1]">Us</span>
         </h1>
         <p className="text-[#ffffff66] font-normal text-[18px]  leading-[22px]">
