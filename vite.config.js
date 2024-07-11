7961fcfd5d4c4578f15c073f88b59a16e226317e
typosbro <typosbro@proton.me>
2024-07-11 23:30:02 +0900

    pwa

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,35 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import path from "path";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  server: {
+    port: 3000,
+  },
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      devOptions: {
+        enabled: true,
+      },
+      workbox: {
+        globPatterns: ["**/*.{js,css,html,ico,png,svg}"],
+      },
+    }),
+  ],
+
+  resolve: {
+    alias: {
+      assets: path.resolve(__dirname, "./src/assets"),
+      components: path.resolve(__dirname, "./src/components"),
+      context: path.resolve(__dirname, "./src/context"),
+      exp: path.resolve(__dirname, "./src/exp"),
+      pages: path.resolve(__dirname, "./src/pages"),
+      styles: path.resolve(__dirname, "./src/styles"),
+      utils: path.resolve(__dirname, "./src/utils"),
+    },
+  },
+});
