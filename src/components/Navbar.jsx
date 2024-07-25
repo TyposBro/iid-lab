dada3f3cfb043907e44e9f1f7c4631e853613bc8
typosbro <typosbro@proton.me>
2024-07-26 00:48:19 +0900

    navbar is ready

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,14 @@
+import MenuButton from "./MenuButton";
+
+import Logo from "../assets/navbar_logo.svg";
+
 export const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div className="flex flex-row px-[30px] py-[25px] bg-background_light">
+      <img src={Logo} alt="Google Logo" className="h-[20px] w-[100px]" />
+      <MenuButton />
+    </div>
+  );
 };
 
 export default Navbar;
