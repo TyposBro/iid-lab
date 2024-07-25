debcc827374c07b5e180fa7a61bd5b4cf0cafbe5
typosbro <typosbro@proton.me>
2024-07-26 00:58:07 +0900

    home

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,8 +4,8 @@ import Logo from "../assets/navbar_logo.svg";
 
 export const Navbar = () => {
   return (
-    <div className="flex flex-row px-[30px] py-[25px] bg-background_light">
-      <img src={Logo} alt="Google Logo" className="h-[20px] w-[100px]" />
+    <div className="flex flex-row justify-between px-[30px] py-[25px] bg-background_light fixed w-dvw">
+      <img src={Logo} alt="IIDL Logo" className="h-[20px] w-[100px]" />
       <MenuButton />
     </div>
   );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,5 @@
 export const Home = () => {
-  return <div>Home</div>;
+  return <div className="w-dvw h-dvh">Home</div>;
 };
 
 export default Home;
