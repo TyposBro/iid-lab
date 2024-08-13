473f8051fec012061395b718ff1d45dd7b33d76c
typosbro <typosbro@proton.me>
2024-08-13 15:16:39 +0900

    init

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-b5f7729d'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.smcfbdkgk5o"
+    "revision": "0.kbufhc0o95g"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
