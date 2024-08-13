ccdd4ba5653f610b73ef5549f4984c41f0d37149
typosbro <typosbro@proton.me>
2024-08-13 16:03:38 +0900

    upt footer arrow

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,15 +1,17 @@
-import Arrow from "../assets/footer_arrow.svg";
-import Email from "../assets/footer_email.svg";
-import Location from "../assets/footer_location.svg";
-import Logo from "../assets/footer_logo.svg";
-import Phone from "../assets/footer_phone.svg";
+import {
+  down_right_dark_arrow,
+  footer_email,
+  footer_location,
+  footer_logo,
+  footer_phone,
+} from "../assets/";
 
 export const Footer = () => {
   return (
     <footer className="w-full h-fit flex flex-col justify-center items-center gap-[80px] bg-text_black_primary relative p-[30px] font-regular">
       <div className="flex flex-row justify-between items-center gap-[20px]">
         <div className="flex flex-col items-start gap-[10px]">
-          <img src={Arrow} className="w-[66px] h-[66px]" />
+          <img src={down_right_dark_arrow} className="w-[66px] h-[66px]" />
           <h1 className="text-[64px] leading-[72px] font-light text-text_white_primary font-special">
             Contact <span className="text-primary_main">Us</span>
           </h1>
@@ -21,21 +23,21 @@ export const Footer = () => {
       </div>
       <div className="flex flex-col items-start gap-[10px] text-text_white_secondary">
         <div className="flex items-center gap-[10px]">
-          <img src={Location} className="w-6 h-6" />
+          <img src={footer_location} className="w-6 h-6" />
           <p className="text-sm">
             #904 Room, 104 Building, UNIST, 50-gil, Eonyang-eup, Ulju-gun, Ulsan, S.Korea
           </p>
         </div>
         <div className="flex items-center gap-[10px]">
-          <img src={Phone} className="w-6 h-6" />
+          <img src={footer_phone} className="w-6 h-6" />
           <p className="text-sm">+82-52-217-2714</p>
         </div>
         <div className="flex items-center gap-[10px] ">
-          <img src={Email} className="w-6 h-6" />
+          <img src={footer_email} className="w-6 h-6" />
           <p className="text-sm">kmyung@unist.ac.kr</p>
         </div>
       </div>
-      <img src={Logo} alt="" />
+      <img src={footer_logo} alt="" />
     </footer>
   );
 };
