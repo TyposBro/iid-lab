541bbcb5fe695b801835fc5add21e94796539703
typosbro <typosbro@proton.me>
2024-08-13 21:39:52 +0900

    navbar bg update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ import Logo from "../assets/navbar_logo.svg";
 
 export const Navbar = () => {
   return (
-    <div className="flex flex-row justify-between px-[30px] py-[25px] bg-background_light fixed w-dvw">
+    <div className="flex flex-row justify-between px-[30px] py-[25px] bg-white fixed z-10 w-dvw">
       <img src={Logo} alt="IIDL Logo" className="h-[20px] w-[100px]" />
       <MenuButton />
     </div>
