11c59e6376c2cf5bc2d49176d0bf3fadb6f95543
typosbro <typosbro@proton.me>
2024-08-13 15:16:56 +0900

    update theme and remove bg

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,14 +6,17 @@ export default {
     fontFamily: {
       regular: ["Inter", "sans-serif"],
       special: ["Space Grotesk", "sans-serif"],
+      super_special: ["Archivo", "monospace"],
     },
     colors: {
       text_white_primary: "#fffffff2",
       text_white_secondary: "#ffffff7f",
       text_white_tertiary: "#ffffff66",
       text_black_primary: "#231F20",
+      text_black_secondary: "#231f20b3",
       primary_main: "#25AAE1",
       background_light: "#e6e6e6",
+      border_dark: "#575757",
     },
   },
   plugins: [],
