64742313faf5cfaa91229f38566c63986712a2a8
typosbro <typosbro@proton.me>
2024-08-19 03:17:17 +0900

    add navigation

--- src/components/MenuButton.jsx
+++ src/components/MenuButton.jsx
@@ -1,7 +1,24 @@
+import { useRef } from "react";
+import { useNavigate } from "react-router-dom";
+
 export const MenuButton = () => {
+  const navigate = useNavigate();
+  const ref = useRef(null);
+
+  const handleNavigation = (path) => {
+    navigate(path);
+    ref.current.checked = false;
+  };
+
   return (
     <>
-      <input type="checkbox" name="navigation" id="nav-toggle" className="nav__checkbox" />
+      <input
+        ref={ref}
+        type="checkbox"
+        name="navigation"
+        id="nav-toggle"
+        className="nav__checkbox"
+      />
       <label htmlFor="nav-toggle" className="nav__btn">
         <span className="nav__icon"> &nbsp; </span>
       </label>
@@ -10,29 +27,29 @@ export const MenuButton = () => {
 
       <nav className="nav__container">
         <ul className="nav__list">
-          <li className="nav__item">
-            <a href="#contact" className="nav__link">
+          <li className="nav__item" onClick={() => handleNavigation("/about")}>
+            <a href="#about" className="nav__link">
               About IIDL
             </a>
           </li>
-          <li className="nav__item">
+          <li className="nav__item" onClick={() => handleNavigation("/projects")}>
             <a href="#projects" className="nav__link">
               Projects
             </a>
           </li>
-          <li className="nav__item">
-            <a href="#bublications" className="nav__link">
+          <li className="nav__item" onClick={() => handleNavigation("/publications")}>
+            <a href="#publications" className="nav__link">
               Publications
             </a>
           </li>
-          <li className="nav__item">
+          <li className="nav__item" onClick={() => handleNavigation("/team")}>
             <a href="#team" className="nav__link">
               Team
             </a>
           </li>
-          <li className="nav__item">
+          <li className="nav__item" onClick={() => handleNavigation("/")}>
             <a href="#news" className="nav__link">
-              News
+              Home
             </a>
           </li>
         </ul>
