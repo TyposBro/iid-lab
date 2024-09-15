34ab19201729f863572595413e32ecea45545e24
typosbro <typosbro@proton.me>
2024-09-15 23:56:45 +0900

    add youtube video intro

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -17,11 +17,11 @@ export const Home = () => {
       <Intro navigate={navigate} />
       <div className="w-full my-[30px]">
         <LiteYouTubeEmbed
-          id="s5vJO6vUeaM" // Default none, id of the video or playlist
+          id="Xd-lcSxIsHM" // Default none, id of the video or playlist
           adNetwork={true} // Default true, to preconnect or not to doubleclick addresses called by YouTube iframe (the adnetwork from Google)
           params="" // any params you want to pass to the URL, assume we already had '&' and pass your parameters string
           playlist={false} // Use true when your ID be from a playlist
-          playlistCoverId="L2vS_050c-M" // The ids for playlists did not bring the cover in a pattern to render so you'll need pick up a video from the playlist (or in fact, whatever id) and use to render the cover. There's a programmatic way to get the cover from YouTube API v3 but the aim of this component is do not make any another call and reduce requests and bandwidth usage as much as possibe
+          playlistCoverId="Xd-lcSxIsHM" // The ids for playlists did not bring the cover in a pattern to render so you'll need pick up a video from the playlist (or in fact, whatever id) and use to render the cover. There's a programmatic way to get the cover from YouTube API v3 but the aim of this component is do not make any another call and reduce requests and bandwidth usage as much as possibe
           poster="hqdefault" // Defines the image size to call on first render as poster image. Possible values are "default","mqdefault",  "hqdefault", "sddefault" and "maxresdefault". Default value for this prop is "hqdefault". Please be aware that "sddefault" and "maxresdefault", high resolution images are not always avaialble for every video. See: https://stackoverflow.com/questions/2068344/how-do-i-get-a-youtube-video-thumbnail-from-the-youtube-api
           title="YouTube Embed" // a11y, always provide a title for iFrames: https://dequeuniversity.com/tips/provide-iframe-titles Help the web be accessible ;)
           noCookie={true} // Default false, connect to YouTube via the Privacy-Enhanced Mode using https://www.youtube-nocookie.com
@@ -30,6 +30,7 @@ export const Home = () => {
           iframeClass="" // Default none, gives control to add a class to iframe element itself
           playerClass="lty-playbtn" // Default as "lty-playbtn" to control player button styles
           wrapperClass="yt-lite rounded-[30px]" // Default as "yt-lite" for the div wrapping the area, the most important class and needs extra attention, please refer to LiteYouTubeEmbed.css for a reference.
+          muted={true}
         />
       </div>
       <Prof />
