c66baaf8c828d96d2e9f917f7969a6e5c8ee3173
typosbro <typosbro@proton.me>
2024-10-12 19:00:53 +0900

    team ready

--- src/pages/Team.jsx
+++ src/pages/Team.jsx
@@ -9,7 +9,14 @@ export const Team = () => {
       <Prof />
       <CurrentTeam />
       <Alumni />
-      <GoTo title="Projects Gallery" link="/gallery" />
+      {window.innerWidth <= 640 ? (
+        <GoTo title="Projects Gallery" link="/gallery" />
+      ) : (
+        <div className="w-full flex justify-between">
+          <GoTo title="Projects Gallery" link="/gallery" />
+          <GoTo title="Lab News" link="/news" />
+        </div>
+      )}
     </div>
   );
 };
@@ -25,9 +32,9 @@ const Prof = () => {
   };
 
   return (
-    <div className="flex flex-col gap-[30px] my-[30px] w-full">
+    <div className="flex flex-col gap-[30px] my-[30px] w-full sm:grid sm:grid-cols-2 sm:grid-rows-2 sm:gap-5 ">
       <div
-        className="rounded-[30px] w-full h-[360px]"
+        className="rounded-[30px] w-full h-[360px] sm:size-96 sm:m-auto sm:col-start-2 row-start-1 row-end-3 sm:self-start"
         style={{ backgroundImage: `url(${prof.img})`, backgroundSize: "cover" }}
       ></div>
       <div className="flex flex-col gap-[5px] w-full">
@@ -37,7 +44,7 @@ const Prof = () => {
         </h1>
         <h3 className="text-[12px] text-text_black_secondary">{prof.desc}</h3>
       </div>
-      <div className="flex flex-col gap-[10px] font-semibold text-[18px]">
+      <div className="flex flex-col gap-[10px] font-semibold text-[18px] sm:flex-row sm:w-1/2">
         <HashLink
           className="flex justify-center items-center gap-[10px] border-2 border-primary_main bg-primary_main border-solid rounded-[15px] w-full h-[50px] text-text_white_primary"
           to="/prof"
@@ -104,7 +111,7 @@ const CurrentTeam = () => {
   return (
     <div className="flex flex-col gap-[30px] py-[30px] w-full">
       <div className="flex flex-col gap-[10px]">
-        <div className="flex items-center">
+        <div className="flex items-center justify-between">
           <h2 className="font-light text-[48px] text-text_black_primary leading-[48px]">
             Current Team
           </h2>
@@ -116,7 +123,7 @@ const CurrentTeam = () => {
           a lot of fun, thanks to our enthusiastic and collaborative team!
         </h3>
       </div>
-      <div className="flex gap-[10px]">
+      <div className="flex gap-[10px] sm:w-1/2 lg:w-1/3">
         <button
           className={`place-content-center border-2 border-primary_main grid active:bg-primary_main border-solid rounded-full w-full h-[30px] text-primary_main active:text-text_white_primary ${
             "All" === selected ? "bg-primary_main text-text_white_primary" : "text-primary_main"
@@ -139,7 +146,7 @@ const CurrentTeam = () => {
         ))}
       </div>
 
-      <div className="flex flex-col items-center gap-[10px]">
+      <div className="flex flex-col items-center gap-[10px] sm:grid sm:grid-cols-2 lg:grid-cols-3 ">
         {members
           .filter((elem) => (selected === "All" ? true : elem.role === selected))
           .map((member) => (
@@ -196,7 +203,7 @@ const Alumni = () => {
         </h2>
         <Down_left_dark_arrow className="size-[46px]" />
       </div>
-      <div className="flex gap-[10px]">
+      <div className="flex gap-[10px] sm:w-1/2 lg:w-1/3">
         <button
           className={`place-content-center border-2 border-primary_main grid active:bg-primary_main border-solid rounded-full w-full h-[30px] text-primary_main active:text-text_white_primary ${
             "All" === selected ? "bg-primary_main text-text_white_primary" : "text-primary_main"
@@ -208,7 +215,7 @@ const Alumni = () => {
         </button>
         {derivedRoles.map((role) => (
           <button
-            className={`place-content-center border-2 border-primary_main grid active:bg-primary_main border-solid rounded-full w-full h-[30px] text-primary_main active:text-text_white_primary ${
+            className={`place-content-center border-2 border-primary_main grid active:bg-primary_main border-solid rounded-full w-full h-[30px] text-primary_main active:text-text_white_primary  ${
               role === selected ? "bg-primary_main text-text_white_primary" : "text-primary_main"
             }`}
             key={role}
@@ -219,7 +226,7 @@ const Alumni = () => {
         ))}
       </div>
 
-      <div className="flex flex-col items-center gap-[10px]">
+      <div className="flex flex-col items-center gap-[10px] sm:grid sm:grid-cols-2 lg:grid-cols-3">
         {members
           .filter((elem) => (selected === "All" ? true : elem.role === selected))
           .map((member) => (
