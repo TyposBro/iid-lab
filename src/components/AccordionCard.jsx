e887eb0d59b2a0b928f78b7ccabd9e2f2314660c
typosbro <typosbro@proton.me>
2025-05-05 03:45:53 +0900

    team linkedin

--- src/components/AccordionCard.jsx
+++ src/components/AccordionCard.jsx
@@ -1,32 +1,32 @@
 import { Down_straight_neutral_arrow } from "@/assets";
 import PropTypes from "prop-types";
 import { useState } from "react";
+import { LinkedIn } from "@mui/icons-material";
 
-const AccordionCard = ({ title, subtitle, desc, bg }) => {
+const AccordionCard = ({ title, subtitle, desc, bg, linkedin }) => {
   const [expanded, setExpanded] = useState(false);
 
   return (
-    <div key={title} className="w-full relative " onClick={() => setExpanded((prev) => !prev)}>
-      <div
-        className="bg-border_dark w-full h-[200px] rounded-[20px] relative "
-        style={{
-          backgroundImage: `url(${bg})`,
-          backgroundSize: "100% 200px",
-          backgroundPosition: "center",
-          backgroundRepeat: "no-repeat",
-        }}
-      >
-        {/* Dark Overlay */}
-        <div className="absolute inset-0 bg-black bg-opacity-10 rounded-[20px]"></div>
-        {/* Dark Overlay */}
-        <div className="w-full flex justify-between items-center absolute bottom-[20px] px-[20px] ">
-          <div>
-            <h2 className="text-text_white_primary font-bold text-[24px] ">{title}</h2>
-            <h3 className="text-primary_main italic  text-[12px] font-medium">{subtitle}</h3>
-          </div>
-          <div className="bg-white rounded-full size-8 grid place-content-center">
+    <div key={title} className="w-full flex flex-col gap-1.5">
+      <img className="w-full h-52 rounded-3xl " src={bg} />
+      <div className="w-full flex justify-between items-center px-4">
+        <div className="font-bold text-black">
+          <h2 className="text-xl">{title}</h2>
+          <h3 className="italic text-base">{subtitle}</h3>
+        </div>
+        <div className="flex justify-between items-center gap-2">
+          {linkedin && (
+            <a href={linkedin} target="_blank" rel="noreferrer noopener">
+              <LinkedIn className="text-text_black_primary hover:text-blue-400 size-7 z-10" />
+            </a>
+          )}
+
+          <div
+            onClick={() => setExpanded((prev) => !prev)}
+            className="bg-black size-7 p-1 rounded-full grid place-content-center"
+          >
             <Down_straight_neutral_arrow
-              className={`size-5 transform origin-center transition duration-500 ease-out text-primary_main ${
+              className={`transform origin-center transition duration-500 ease-out text-white ${
                 expanded ? "rotate-180" : ""
               }`}
             />
@@ -35,8 +35,8 @@ const AccordionCard = ({ title, subtitle, desc, bg }) => {
       </div>
 
       <div
-        className={`grid px-[10px] transition-all duration-700 ease-in-out text-border_dark ${
-          expanded ? "grid-rows-[1fr] py-[15px] opacity-100" : "grid-rows-[0fr]  opacity-0"
+        className={`grid text-sm px-4 transition-all duration-700 ease-in-out text-border_dark ${
+          expanded ? "grid-rows-[1fr] opacity-100" : "grid-rows-[0fr]  opacity-0"
         }`}
       >
         <div className="overflow-hidden">{desc}</div>
@@ -50,6 +50,7 @@ AccordionCard.propTypes = {
   subtitle: PropTypes.string.isRequired,
   desc: PropTypes.string.isRequired,
   bg: PropTypes.string.isRequired,
+  linkedin: PropTypes.string.isRequired,
 };
 
 export default AccordionCard;
